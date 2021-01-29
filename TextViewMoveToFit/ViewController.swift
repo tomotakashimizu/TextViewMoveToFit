59b72ab7f6ca45333834acee66c30823021286ec
tomotakashimizu <tomotaka.developer@gmail.com>
2021-01-29 17:21:54 +0900

    Initial Commit

---
+++ TextViewMoveToFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextViewMoveToFit
+//
+//  Created by 清水智貴 on 2021/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
